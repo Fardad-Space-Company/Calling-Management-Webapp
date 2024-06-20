224e3956520ffbdee1c88ca96cca572e988628f2
Hamide Ebrahimiyan <131973666+hamideebrahimiyan@users.noreply.github.com>
2024-06-20 20:21:31 +0330

    Adding employeeID to user and define condition for that in admin page

--- saleseeker/management/admin.py
+++ saleseeker/management/admin.py
@@ -1,2 +1,7 @@
 from django.contrib import admin
+from .models import EmployeeID
 
+class EmployeeIDAdmin(admin.ModelAdmin):
+    list_display = ['user', 'employee_id']  # Specify fields to display in the admin list view
+
+admin.site.register(EmployeeID, EmployeeIDAdmin)
