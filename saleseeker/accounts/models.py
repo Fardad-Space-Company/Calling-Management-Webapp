348390b664dc568a1b644cede7709cb517d42584
miladagdam <miladagdam@gmail.com>
2024-06-24 15:52:27 +0330

    fix default image

--- saleseeker/accounts/models.py
+++ saleseeker/accounts/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.hashers import make_password
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    image = models.ImageField(upload_to='profile_pics', default='default.jpg')
+    image = models.ImageField(upload_to='profile_pics', default='profile_pics/default.jpg')
     rule = models.CharField(max_length=100, default='Default Rule')
     email = models.EmailField(max_length=100, blank=True)
     # Extra password field, use cautiously considering security practices
